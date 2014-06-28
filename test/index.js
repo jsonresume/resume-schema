0e67a7266157a534e0b56f984a2e431eced6ac39
Thomas Davis <thomasalwyndavis@gmail.com>
2014-06-29 09:28:01 +1000

    fleshed out schematic

--- test/index.js
+++ test/index.js
@@ -3,8 +3,8 @@ var validator = require('../validator');
 
 test('Test validator on resume.json.', function(t) {
     validator.validate(validator.resumeJson, function(report, err) {
-        t.equal(err, null, 'no error');
-        t.equal(report && report.valid, true, 'schema passed.');
+        t.equal(err, null, 'No formatting errors');
+        t.equal(report && report.valid, true, 'Passes JsonResume v1.0.0 specification - DRAFT.');
         t.end();
     });
 });
