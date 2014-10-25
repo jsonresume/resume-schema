a65964af4b9668cafff13307420794c1b297ee54
Spencer Elliott <me@elliottsj.com>
2014-10-24 22:36:15 -0400

    Add custom date validator
    Allows matching YYYY-MM-DD, YYYY-MM, or just YYYY

--- validator.js
+++ validator.js
@@ -3,6 +3,22 @@ var fs = require('fs');
 var path = require('path');
 var resumeJson = require('./resume');
 
+
+/* Register custom validators */
+ZSchema.registerFormat('resume-date', function (date) {
+  if (typeof date !== 'string') {
+    return true;
+  }
+  var matches = /^[0-9]{4}(-([0-9]{2})(-([0-9]{2}))?)?$/.exec(date);
+  if (matches === null) {
+    return false
+  }
+  var month = matches[2];
+  var day = matches[4];
+  return !(month < '01' || month > '12' || day < '01' || day > '31');
+});
+
+
 // TODO - Remove this sync call
 var schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'schema.json'), 'utf8'));
 
@@ -19,4 +35,4 @@ function validate(resumeJson, callback) {
 module.exports = {
     validate: validate,
     resumeJson: resumeJson
-}
+};
