d00fe2adae58e2735ee7692d1aa901f4bfaac976
Matthew J. Martin <matthew.mar10@gmail.com>
2015-08-27 16:30:56 +0700

    use CPS for error handling; add test case for invalid resume

--- test/index.js
+++ test/index.js
@@ -1,10 +1,20 @@
 var test = require('tape');
 var validator = require('../validator');
 
-test('Test validator on resume.json.', function(t) {
-    validator.validate(validator.resumeJson, function(report, err) {
-        t.equal(err, null, 'No formatting errors');
-        t.equal(report && report.valid, true, 'Passes JsonResume v1.0.0 specification - DRAFT.');
-        t.end();
-    });
+test('Validates a valid resume', function(t) {
+  validator.validate(validator.resumeJson, function(err, report) {
+    t.equal(err, null, 'No formatting errors');
+    t.equal(report && report.valid, true, 'Passes JsonResume v1.0.0 specification - DRAFT.');
+    t.end();
+  });
+});
+
+test('Validates an invalid resume', function(t) {
+  validator.validate({
+    basics: [],
+    profiles: {}
+  }, function(err) {
+    t.ok(err, 'Error is triggered by validation');
+    t.end();
+  });
 });
