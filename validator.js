a655a4ea47d9f8197d9a7b5f8d2e42e1984e1f98
Ben Axnick <ben@axnick.com.au>
2017-02-19 21:46:34 +1100

    Use ZSchema 3.x while preserving 2.4.x return structure (#279)

--- validator.js
+++ validator.js
@@ -8,11 +8,16 @@ var path = require('path');
 var schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'schema.json'), 'utf8'));
 
 function validate(resumeJson, callback) {
-  ZSchema.validate(resumeJson, schema)
-    .then(function(report) {
-      callback(null, report);
-    })
-    .catch(callback);
+  // Callers expect z-schema 2.4.x report object
+  var callbackWrapper = function(err, valid) {
+    if(err) {
+      callback(err)
+    } else {
+      callback(null, {valid: valid});
+    }
+  }
+
+  new ZSchema().validate(resumeJson, schema, callbackWrapper);
 }
 
 module.exports = {
