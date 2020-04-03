0d7df1a364db8bcec64b1a51d88f06cec3829026
antialias <thomas.hallock@compass.com>
2020-04-02 19:59:39 -0700

    proxy args directly to validator, allowing for choice between callback and promise api

--- validator.js
+++ validator.js
@@ -2,19 +2,9 @@
 var ZSchema = require('z-schema');
 var schema = require('./schema');
 
-function validate(resumeJson, callback) {
-  var callbackWrapper = function(err, valid) {
-    if(err) {
-      callback(err)
-    } else {
-      callback(null, {valid: valid});
-    }
-  }
-
-  new ZSchema().validate(resumeJson, schema, callbackWrapper);
-}
+const zs = new ZSchema();
 
 module.exports = {
-  validate: validate,
+  validate: (resumeJson, ...args) => zs.validate(resumeJson, schema, ...args),
   schema: schema
 };
