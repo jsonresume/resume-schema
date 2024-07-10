ec13c2ec656b412151be34cf3a43fb0a113aa158
Ajax Davis <thomasalwyndavis@gmail.com>
2024-07-10 20:57:44 +1000

    fix: correctly use new validator in date specs

--- test/dates.spec.js
+++ test/dates.spec.js
@@ -1,54 +1,56 @@
-var test = require('tape');
-var ZSchema = require('z-schema');
-const fixtures = require('./__test__/dates.json');
+var test = require("tape");
+var Validator = require("jsonschema").Validator;
+
+const fixtures = require("./__test__/dates.json");
 // var mockDateSchema = require('./__test__/mockDateSchema.json');
 
 const mockDateSchema = {
-  "type": "string",
-  "description": "Mock Date Format",
-  "pattern": "^([1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]|[1-2][0-9]{3}-[0-1][0-9]|[1-2][0-9]{3})$"
+  type: "string",
+  description: "Mock Date Format",
+  pattern:
+    "^([1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]|[1-2][0-9]{3}-[0-1][0-9]|[1-2][0-9]{3})$",
 };
 
 function dateValidate(resumeJson, callback) {
-  var callbackWrapper = function(err, valid) {
-    if(err) {
-      callback(err)
-    } else {
-      callback(null, {valid: valid});
-    }
+  var v = new Validator();
+
+  const validation = v.validate(resumeJson, mockDateSchema);
+
+  if (!validation.valid) {
+    return callback(validation.errors, false);
   }
 
-  new ZSchema().validate(resumeJson, mockDateSchema, callbackWrapper);
+  return callback(null, true);
 }
 
-test('dates - YYYY-MM-DD', (t) => {
+test("dates - YYYY-MM-DD", (t) => {
   dateValidate(fixtures.yearMonthDay, (err, valid) => {
-    t.equal(err, null, 'err should be null');
-    t.true(valid, 'valid is true');
+    t.equal(err, null, "err should be null");
+    t.true(valid, "valid is true");
   });
   t.end();
 });
 
-test('dates - YYYY-MM', (t) => {
+test("dates - YYYY-MM", (t) => {
   dateValidate(fixtures.yearMonth, (err, valid) => {
-    t.equal(err, null, 'err should be null');
-    t.true(valid, 'valid is true');
+    t.equal(err, null, "err should be null");
+    t.true(valid, "valid is true");
   });
   t.end();
 });
 
-test('dates - YYYY', (t) => {
+test("dates - YYYY", (t) => {
   dateValidate(fixtures.yearMonthDay, (err, valid) => {
-    t.equal(err, null, 'err should be null');
-    t.true(valid, 'valid is true');
+    t.equal(err, null, "err should be null");
+    t.true(valid, "valid is true");
   });
   t.end();
 });
 
-test('dates - invalid', (t) => {
+test("dates - invalid", (t) => {
   dateValidate(fixtures.invalid, (err, valid) => {
-    t.notEqual(err, null, 'err should contain an error');
-    t.false(valid, 'valid is false');
+    t.notEqual(err, null, "err should contain an error");
+    t.false(valid, "valid is false");
   });
   t.end();
 });
